9645bf88221c20ae85e3c4ccb2a776c6061205cc
Yohan-Rodriguez <85138862+Yohan-Rodriguez@users.noreply.github.com>
2022-11-01 14:59:37 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,12 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 4) {
+        basic.showString("TC")
+    }
+})
 input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(1)
+    radio.sendNumber(4)
     basic.showIcon(IconNames.Yes)
     basic.pause(25000)
     basic.showIcon(IconNames.No)
 })
-radio.setGroup(100)
+radio.setGroup(50)
